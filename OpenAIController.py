501cdb7fcc66508e398ac6967a9f0064236b9c83
Matias <matias.szbar@gmail.com>
2021-05-14 23:34:26 +0200

    Add gitignore

--- OpenAIController.py
+++ OpenAIController.py
@@ -1,3 +1,7 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class OpenAIController(object):
 
@@ -5,4 +9,17 @@ class OpenAIController(object):
         print("OpenAI Controller created")
 
     def foo(self):
-        print("Controller dummy hello world")
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt="Write a creative ad for the following product to run on Facebook:\n\"\"\"\"\"\"\nAiree is a line "
+                   "of skin-care products for young women with delicate skin. The ingredients are "
+                   "all-natural.\n\"\"\"\"\"\"\nThis is the ad I wrote for Facebook aimed at teenage "
+                   "girls:\n\"\"\"\"\"\"",
+            temperature=0.5,
+            max_tokens=60,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=["\"\"\"\"\"\""]
+        )
+        print(response)
