7c190f448c732c9ede644fa6da5b16208750f380
Mohamed Benali <cg3mbenali@gmail.com>
2021-05-14 22:45:12 +0200

    Create dummy OpenAI Controller

---
+++ OpenAIController.py
@@ -0,0 +1,8 @@
+
+class OpenAIController(object):
+
+    def __init__(self):
+        print("OpenAI Controller created")
+
+    def foo(self):
+        print("Controller dummy hello world")
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-
-
-
-
+from OpenAIController import OpenAIController
 
 if __name__ == '__main__':
     # execute only if run as the entry point into the program
+
+    openAI = OpenAIController()
+    openAI.foo()
     print("hello world")
