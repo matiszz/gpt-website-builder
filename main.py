7d855b0f6691775a8df17fcc412382bbc1fa2282
Mohamed Benali <cg3mbenali@gmail.com>
2021-05-15 01:11:40 +0200

    open api Add testimonial bio

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from OpenAIController import OpenAIController
 sample_description = "Velox is a real-time platform that helps remote teams keeping organized chats. In Velox, " \
                      "you can create new conversations for different topics. "
 
+
 if __name__ == '__main__':
     # execute only if run as the entry point into the program
 
     openAI = OpenAIController()
-    openAI.get_tagline(sample_description)
+    #openAI.get_tagline(sample_description)
+
+    openAI.get_sample_testimonial_bio(sample_description)
