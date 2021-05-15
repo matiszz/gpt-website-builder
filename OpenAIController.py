9a47222596196c740347440293424307117bc4ba
Mohamed Benali <cg3mbenali@gmail.com>
2021-05-15 02:12:34 +0200

    Testimonial name and role done

--- OpenAIController.py
+++ OpenAIController.py
@@ -11,6 +11,17 @@ class OpenAIController(object):
     def __init__(self):
         print("OpenAI Controller created")
 
+    ### HELPERS ###
+    @staticmethod
+    def list2string(list_of_strings):
+        result = ""
+        for name in list_of_strings:
+            result = result + name + "\n" + SEPARATOR + "\n"
+        return result
+
+    ### METHODS ###
+
+
     @staticmethod
     def get_tagline(description):
         instruction = "Write a copy title for the following product:"
@@ -39,6 +50,46 @@ class OpenAIController(object):
             top_p=1,
             frequency_penalty=0.1+0.3,
             presence_penalty=0.48+0.3,
+            stop=[SEPARATOR]
+        )
+        print(response.choices[0].text)
+
+    @staticmethod
+    def get_sample_testimonial_name(sample_testimonial_names):
+        description = OpenAIController.list2string(sample_testimonial_names)
+        #print(description)
+        instruction = "This is a name:"
+        #print(instruction)
+        action = "This is a name:"
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=instruction + SEPARATOR + description + '\n' + SEPARATOR + '\n' + action + SEPARATOR,
+            temperature=0.5 + 0.4,
+            max_tokens=60,
+            top_p=1,
+            frequency_penalty=0.1+0.4,
+            presence_penalty=0.48+0.4,
+            stop=[SEPARATOR]
+        )
+        print(response.choices[0].text)
+
+
+
+    @staticmethod
+    def get_sample_testimonial_role(sample_testimonial_names):
+        description = OpenAIController.list2string(sample_testimonial_names)
+        #print(description)
+        instruction = "This is a role:"
+        #print(instruction)
+        action = "This is a role:"
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=instruction + SEPARATOR + description + '\n' + SEPARATOR + '\n' + action + SEPARATOR,
+            temperature=0.5 + 0.4,
+            max_tokens=60,
+            top_p=1,
+            frequency_penalty=0.1+0.4,
+            presence_penalty=0.48+0.4,
             stop=[SEPARATOR]
         )
         print(response.choices[0].text)
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from OpenAIController import OpenAIController
 sample_description = "Velox is a real-time platform that helps remote teams keeping organized chats. In Velox, " \
                      "you can create new conversations for different topics. "
 
+sample_testimonial_names = ["Martha Taylor", "Lucy Williams", "John Smith", "Kevin Brown"]
+sample_testimonial_roles = ["Senior Product Designer", "UI Develeoper", "CEO", "CIO", "DESIGNER"]
+
 
 if __name__ == '__main__':
     # execute only if run as the entry point into the program
@@ -10,4 +13,9 @@ if __name__ == '__main__':
     openAI = OpenAIController()
     #openAI.get_tagline(sample_description)
 
-    openAI.get_sample_testimonial_bio(sample_description)
+    #openAI.get_sample_testimonial_bio(sample_description)
+    print("---")
+    #openAI.get_sample_testimonial_name(sample_testimonial_names)
+    print("---")
+    #openAI.get_sample_testimonial_role(sample_testimonial_roles)
+
