dcd2d26b08739b8232b531cc7705d8db1aee21cc
MIDD\joff <joffre@pm.me>
2021-05-15 02:38:55 +0200

    Added pexels API

---
+++ PexelsController.py
@@ -0,0 +1,19 @@
+import pprint
+
+from dotenv import dotenv_values
+from pexels_api import API
+
+PEXELS_API_KEY = dotenv_values(".env")['PEXELS_API_KEY']
+# Create API object
+api = API(PEXELS_API_KEY)
+
+
+class PexelsController(object):
+    def __init__(self):
+        print("Pexels Controller created")
+
+    @staticmethod
+    def search_photo(photo, size):
+        api.search(photo, page=1, results_per_page=1)
+        photos_result = api.get_entries()
+        return getattr(photos_result[0], size)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from OpenAIController import OpenAIController
+from PexelsController import PexelsController
 
 sample_description = "Velox is a real-time platform that helps remote teams keeping organized chats. In Velox, " \
                      "you can create new conversations for different topics. "
@@ -7,7 +8,9 @@ sample_description = "Velox is a real-time platform that helps remote teams keep
 if __name__ == '__main__':
     # execute only if run as the entry point into the program
 
-    openAI = OpenAIController()
+    #openAI = OpenAIController()
     #openAI.get_tagline(sample_description)
 
-    openAI.get_sample_testimonial_bio(sample_description)
+    #openAI.get_sample_testimonial_bio(sample_description)
+    pexels = PexelsController()
+    photo = pexels.search_photo("barcelona", "medium")
