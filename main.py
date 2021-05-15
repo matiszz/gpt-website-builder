9e3a2367d596232f93ddba72943a0840ac2d4eec
Matias <matias.szbar@gmail.com>
2021-05-16 00:21:05 +0200

    Check magic key

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import flask
 from OpenAIController import OpenAIController
 from HTMLGenerator import HTMLGenerator
 from PexelsController import PexelsController
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
+from dotenv import dotenv_values
 
 app = Flask(__name__)
+MAGIC_KEY = dotenv_values(".env")['MAGIC_KEY']
 
 sample_info = {
     'description': "Velox is a real-time platform that helps remote teams keeping organized chats. In Velox, can create new conversations for different topics. It also allows you to create different domain levels and organize users in addresses. ",
@@ -29,26 +31,31 @@ def hello():
 
     form = request.json['form_response']['answers']
 
-    info = {
-        'description': list(filter(lambda field: field['field']['id'] == 'MXo1rWpND8vZ', form))[0]['text'],
-        'product_type': list(filter(lambda field: field['field']['id'] == 'cPauKwCscbCk', form))[0]['text'],
-        'address': list(filter(lambda field: field['field']['id'] == 'R4aBPISAiiTO', form))[0]['text'],
-        'email': list(filter(lambda field: field['field']['id'] == 'lTFrpytOV3f5', form))[0]['email'],
-        'phone_number': list(filter(lambda field: field['field']['id'] == 'OtMQqEttgCq9', form))[0]['phone_number'],
-        'web_name': list(filter(lambda field: field['field']['id'] == 'cBdcNK94ys9R', form))[0]['text']
-    }
+    provided_key = list(filter(lambda field: field['field']['id'] == 'ZKyWAMzdVY9h', form))[0]['text']
 
-    blocks = openAI.get_landing_blocks(info['product_type'])
+    if provided_key == MAGIC_KEY:
+        info = {
+            'description': list(filter(lambda field: field['field']['id'] == 'MXo1rWpND8vZ', form))[0]['text'],
+            'product_type': list(filter(lambda field: field['field']['id'] == 'cPauKwCscbCk', form))[0]['text'],
+            'address': list(filter(lambda field: field['field']['id'] == 'R4aBPISAiiTO', form))[0]['text'],
+            'email': list(filter(lambda field: field['field']['id'] == 'lTFrpytOV3f5', form))[0]['email'],
+            'phone_number': list(filter(lambda field: field['field']['id'] == 'OtMQqEttgCq9', form))[0]['phone_number'],
+            'web_name': list(filter(lambda field: field['field']['id'] == 'cBdcNK94ys9R', form))[0]['text']
+        }
 
-    keywords = openAI.get_image_keywords(info['description'])
-    print('KEYWORDS')
-    print(keywords)
-    info['photo1'] = pexels.search_photo(keywords, "large", 1)
-    info['photo2'] = pexels.search_photo(keywords, "large", 2)
+        blocks = openAI.get_landing_blocks(info['product_type'])
 
-    htmlGen.create_result_file(blocks, info)
+        keywords = openAI.get_image_keywords(info['description'])
+        print('KEYWORDS')
+        print(keywords)
+        info['photo1'] = pexels.search_photo(keywords, "large", 1)
+        info['photo2'] = pexels.search_photo(keywords, "large", 2)
 
-    return '200'
+        htmlGen.create_result_file(blocks, info)
+
+        return '200'
+    else:
+        abort(403)
 
 
 if __name__ == '__main__':
